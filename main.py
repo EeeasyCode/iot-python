93bdeb25c68c168b5a096c05f2e79b815a4a660b
EeeasyCode <eeeasycode@gmail.com>
2023-10-15 03:07:42 +0900

    [FEAT] LED ON/OFF 예제 demo

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from tkinter import *
 
+
 class WidgetDemo:
     def __init__(self):
         window = Tk()
@@ -7,46 +8,38 @@ class WidgetDemo:
 
         frame1 = Frame(window)
         frame1.pack()
-        self.v1 = IntVar()
-        cbtBold = Checkbutton(frame1, text="굵게",
-            variable=self.v1, command=self.processCheckbutton)
         self.v2 = IntVar()
-        rbRed = Radiobutton(frame1, text="빨간색", bg="red",
-            variable=self.v2, value=1,
-            command=self.processRadiobutton)
-        rbYellow = Radiobutton(frame1, text="노란색", bg="yellow",
-                            variable=self.v2, value=2,
-                            command=self.processRadiobutton)
-        cbtBold.grid(row=1, column=1)
-        rbRed.grid(row=1, column=2)
-        rbYellow.grid(row=1, column=3)
+        led_on = Radiobutton(frame1, text="LED ON", variable=self.v2, value=1, command=self.process_radio_button)
+        led_off = Radiobutton(frame1, text="LED OFF", variable=self.v2, value=2, command=self.process_radio_button)
+
+        led_on.grid(row=1, column=1)
+        led_off.grid(row=1, column=2)
 
         frame2 = Frame(window)
         frame2.pack()
-        label=Label(frame2, text="입력하세요: ")
-        self.name = StringVar()
-        entryName=Entry(frame2, textvariable=self.name)
-        btGetName=Button(frame2, text="가져오기", command=self.processButton)
-        message = Message(frame2, text="위젯 데모입니다.")
+        label = Label(frame2, text="LED 색을 입력하세요(R/G/B): ")
+        self.ledColor = StringVar()
+        entry_name = Entry(frame2, textvariable=self.ledColor)
+        btn_get_name = Button(frame2, text="실행", command=self.process_button)
+        self.message = Message(frame2, text="위젯 데모입니다.")
         label.grid(row=1, column=1)
-        entryName.grid(row=1, column=2)
-        btGetName.grid(row=1, column=3)
-        message.grid(row=1, column=4)
+        entry_name.grid(row=1, column=2)
+        btn_get_name.grid(row=1, column=3)
+        self.message.grid(row=1, column=4)
 
         text = Text(window)
         text.pack()
-        text.insert(END, "ㅁㄴㅇㄹㄴㅁㅇㄹ")
-        text.insert(END, "ㅁㄴㅇㄹㄴㅁㅇㄹ")
-        text.insert(END, "ㅁㄴㅇㄹㄴㅁㅇㄹ")
         window.mainloop()
 
-    def processCheckbutton(self):
-        print("체크 버튼이" + ("선택") if self.v1.get() == 1 else "해제")
+    def process_radio_button(self):
+        print("LED ON" if self.v2.get() == 1 else "LED OFF")
 
-    def processRadiobutton(self):
-        print("빨간색" if self.v1.get() == 1 else "노란색")
+    def process_button(self):
+        print(self.ledColor.get() + " LED")
+        if self.v2.get() == 1:
+            self.message.configure(text="LED ON")
+        else:
+            self.message.configure(text="LED OFF")
 
-    def processButton(self):
-        print("이름은 " + self.name.get() + "입니다.")
 
 WidgetDemo()
